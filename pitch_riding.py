ce9c8f5f997a3ecdd3c1ec12212e39aba01190d3
Timofei Kondratev <t.a.kondratiev@gmail.com>
2025-06-22 17:30:15 +0200

    Update pitch_riding script

--- pitch_riding.py
+++ pitch_riding.py
@@ -5,8 +5,8 @@ from functions.check_config import check_config
 from functions.rekordjog_start_sequence import rekordjog_start_sequence
 
 
-# The JOG_MULTIPLIER is required for smooth jog operation. 
-# Pioneer controllers seem to be sending MIDI signal at a much higher rate than XONE:4D. 
+# The JOG_MULTIPLIER is required for smooth jog operation.
+# Pioneer controllers seem to be sending MIDI signal at a much higher rate than XONE:4D.
 # If you send one fake Pioneer message for each Xone jog message, scratching sounds unnatural.
 # You need to test different values for other controllers.
 JOG_MULTIPLIER = 1
@@ -18,27 +18,24 @@ tempo_values = [
     [63, 63],
 ]
 
-JOG_CODES = {
-    (0xB0, 0x05):0,
-    (0xB0, 0x25):1,
-    (0xB0, 0x45):2,
-    (0xB0, 0x65):3,
-}
 
 TEMPO_BIG_CODES = {
-    (0xbf, 0x11):0,
-    (0xbe, 0x11):1,
-    (0xbf, 0x1f):2,
-    (0xbe, 0x1f):3,
-    (0xbf, 0x13):4,
-    (0xbe, 0x13):5,
-    (0xbf, 0x1d):6,
-    (0xbe, 0x1d):7,
+    (0xBF, 0x11): 0,
+    (0xBE, 0x11): 1,
+    (0xBF, 0x1F): 2,
+    (0xBE, 0x1F): 3,
+    (0xBF, 0x13): 4,
+    (0xBE, 0x13): 5,
+    (0xBF, 0x1D): 6,
+    (0xBE, 0x1D): 7,
 }
 
 
 class PitchRider:
     """
+
+    NOTES AND KNOWLEDGE BASE:
+
     It is assumed that tempo range is set to WIDE in Rekordbox.
     WIDE range goes +-100%.
 
@@ -47,105 +44,165 @@ class PitchRider:
         When msb==0 and lsb==0 the tempo is 0%.
         When msb==0x7f and lsb==0x7f the tempo is 200%.
 
-
-    TEXT DUMP:
     0x2000 is 8192 in decimal. That's ~ half of 16383 which is max 14 bit value.
+    tempo_fader_lsb can be represented by a separate fader to achieve hi-res pitch bend.
 
+    Jog send rate to achieve 200% of playback speed = 792.
+    Messages per second (and per deck). Formula is: 720/(60/33)*2=792.
+    DDJ FLX-4 sends 720 messages per revolution.
+    33 is the speed of a turntable. 60 is seconds in one minute.
+    The result is multiplied by 2 because the maximum scratching speed with current approach is +100%, or 2x.
     """
 
     def __init__(self):
 
         # SETTINGS
+
         self.should_reverse_tempo_fader = False
 
+        # The following only applies to certain types of jogs.
+        # The other type sends messages with a capped rate and increased magnitude (data byte)
+        self.jog_messages_per_revolution = 128
+
+
+
         # RESOURCES
-        self.tempos_roster = [0.06, 0.1, 0.16, 1.0] # For ranges 6%, 10%, 16%, and WIDE (100%) the values are 0.06, 0.1, 0.16, and 1.0 accordingly.
 
-        # INTERNAL STATE      
-        self.tempo_range = 0.1 # Tempo range that the user controls.
-        self.tempo = 1.0 # Tempo is represented as float for convenience.
-        self.pitch_amount = 0.0 # 0.0 for the neutral, -1.0 for the slowest, 1.0 for the fastest tempo.
-        self.nudge_amount = 0.0 # Increases or decreases when the jog is rotating.
+        self.tempo_range_options = [  # For ranges 6%, 10%, 16%, and WIDE (100%) the values are 0.06, 0.1, 0.16, and 1.0 accordingly.
+            0.06,
+            0.1,
+            0.16,
+            1.0,
+        ]
 
-        # MIDI VALUES
-        self.tempo_fader_msb = 0 # Physical tempo fader MSB.
-        self.tempo_fader_lsb = 0 # Physical tempo fader LSB.
-        # tempo_fader_lsb can be represented by a separate fader to achieve hi-res pitch bend.
+        # Below are the first two bytes of controller-specific MIDI messages for jog rotation (decks 1 to 4 are indexed as 0 to 3).
+        # The third byte carries value that indicates speed and direction of rotation. Different controllers use different ways to encode this value.
+        self.jog_turn_codes = {
+            (0xB0, 0x05): 0,  # Deck 1.
+            (0xB0, 0x25): 1,  # Deck 2.
+            (0xB0, 0x45): 2,  # Deck 3.
+            (0xB0, 0x65): 3,  # Deck 4.
+        }
+        self.jog_middle_value = 0x40
+        self.jog_touch_on_codes = {
+            # TODO Replace with actual values
+            (0x9F, 0x26): 0,  # Deck 1.
+            (0x9F, 0x46): 1,  # Deck 2.
+            (0x9E, 0x26): 2,  # Deck 3.
+            (0x9E, 0x46): 3,  # Deck 4.
+        }
+        self.jog_touch_off_codes = {
+            # TODO Replace with actual values
+            (0x8F, 0x26): 0,  # Deck 1.
+            (0x8F, 0x46): 1,  # Deck 2.
+            (0x8E, 0x26): 2,  # Deck 3.
+            (0x8E, 0x46): 3,  # Deck 4.
+        }
 
+        # INTERNAL STATE
 
+        self.tempo = [  # Tempo is represented as float for convenience.
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+        ]
+        self.tempo_range = [  # Tempo range that the user controls. For each deck.
+            self.tempo_range_options[1],
+            self.tempo_range_options[1],
+            self.tempo_range_options[1],
+            self.tempo_range_options[1],
+        ]
 
+        self.pitch_amount = [  # 0.0 for the neutral, -1.0 for the slowest, 1.0 for the fastest tempo.
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+        self.nudge_amount = [  # Increases or decreases when the jog is rotating. Goes back to 0 when the rotation stops.
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+        self.jog_touch = [
+            False,
+            False,
+            False,
+            False,
+        ]
 
-    def select_next_tempo_range(self):
+    def select_next_tempo_range(self, deck_id):
         """
-        Iterate over TEMPOS_ROSTER
+        Iterate over self.tempo_range_options
         """
-        i = self.tempos_roster.index(self.tempo_range)
-        # 3 in this case is the last index in TEMPOS_ROSTER.
-        self.tempo_range = self.tempos_roster[i + 1] if i < 3 else self.tempos_roster[0]
+        i = self.tempo_range_options.index(self.tempo_range[deck_id])
+        # 3 in this case is the last index in self.tempo_range_options.
+        self.tempo_range[deck_id] = (
+            self.tempo_range_options[i + 1] if i < 3 else self.tempo_range_options[0]
+        )
 
-    def process_jog(self):
-        pass
-
-    def tempo_transformation(self):
+    def tempo_transformation(self, deck_id):
         """
         Apply pitch first, nudge second.
         """
 
-        self.tempo = (1.0 + (self.pitch_amount * self.tempo_range)) * (1.0 + self.nudge_amount)
+        self.tempo[deck_id] = (
+            1.0 + (self.pitch_amount[deck_id] * self.tempo_range[deck_id])
+        ) * (1.0 + self.nudge_amount[deck_id])
+
+    def jog(self, msg):
+        """
+        Process jog nudging.
+        """
+        deck_id = self.jog_turn_codes[tuple(msg.bytes()[:2])]
+        v = msg.bytes()[2]  # Value from jog
+
+        self.nudge_amount[deck_id] += self.get_jog_value_delta(v)
+
+    def get_jog_value_delta(self, value):
+        """
+        The current implementation only works for jogs with middle value of 0x40.
+        """
+        return self.jog_middle_value - value
 
 
 
-
-
-
-def tempo_to_msb_lsb(tempo):
-    pass
-
-
-
-
-def jog(midi_out, msg):
-    deck_id = JOG_CODES[tuple(msg.bytes()[:2])]
-    v = msg.bytes()[2]
-
-    ms = mido.Message.from_bytes([176+deck_id, 0x22, v])
-
-    for _ in range(JOG_MULTIPLIER):
-        midi_out.send(ms)
-        # TODO
-        print(ms)
 
 def tempo(midi_out, deck_id):
-    msb = mido.Message.from_bytes([0xB0+deck_id, 0x00, tempo_values[deck_id][0]])
-    lsb = mido.Message.from_bytes([0xB0+deck_id, 0x20, tempo_values[deck_id][1]])
+    msb = mido.Message.from_bytes([0xB0 + deck_id, 0x00, tempo_values[deck_id][0]])
+    lsb = mido.Message.from_bytes([0xB0 + deck_id, 0x20, tempo_values[deck_id][1]])
     midi_out.send(msb)
     midi_out.send(lsb)
+
 
 def main():
     midi_inp, midi_out = check_config()
     try:
         midi_inp_conf, midi_out_conf = check_config()
         midi_inp = mido.open_input(midi_inp_conf)
-        if os.name == 'nt':
+        if os.name == "nt":
             midi_out = mido.open_output(midi_out_conf)
         else:
             midi_out = mido.open_output("RekordJog", True)
-        
+
         rekordjog_start_sequence()
         while True:
-                ims = midi_inp.receive()
-                ims_2b = tuple(ims.bytes()[:2])
+            ims = midi_inp.receive()
+            ims_2b = tuple(ims.bytes()[:2])
 
-                if ims_2b in JOG_CODES:
-                    jog(midi_out, ims)
+            if ims_2b in JOG_CODES:
+                jog(midi_out, ims)
 
-                elif ims_2b in TEMPO_BIG_CODES:
-                    deck_id = math.floor(TEMPO_BIG_CODES[ims_2b] / 2)
-                    tempo_values[deck_id][0] = 127 - ims.bytes()[2]
-                    tempo(midi_out, deck_id)
+            elif ims_2b in TEMPO_BIG_CODES:
+                deck_id = math.floor(TEMPO_BIG_CODES[ims_2b] / 2)
+                tempo_values[deck_id][0] = 127 - ims.bytes()[2]
+                tempo(midi_out, deck_id)
 
     except KeyboardInterrupt:
         print("\nClosing RekordJog, bye.")
+
 
 if __name__ == "__main__":
     main()
